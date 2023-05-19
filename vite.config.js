09d216c89d0f8f56872a4078fed986974cd0c3fa
Maycon Negrini <maycon128@hotmail.com>
2023-05-19 16:52:10 -0300

    slowness fix

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
     vuetify({
       autoImport: true,
-      styles: { configFile: 'src/styles/variables.scss' }
+      // styles: { configFile: 'src/styles/variables.scss' } // causa lentidão ao startar por algum motivo
     }),
   ],
   define: { 'process.env': {} },
